fac11e891f34bc4822fb0174396d894484777c0d
Batman(Квашнин) <124182220+btmnnmtb@users.noreply.github.com>
2023-11-13 08:25:38 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿
+class Menu
+{
+
+
+    public int Show()
+    {
+        int pos = 3;
+        ConsoleKeyInfo key;
+
+        key = Console.ReadKey();
+        while (key.Key != ConsoleKey.Enter)
+        {
+             Console.SetCursorPosition(0, pos);
+            Console.WriteLine("  ");
+            if (key.Key == ConsoleKey.UpArrow)
+            {
+                pos--;
+            }
+            else if (key.Key == ConsoleKey.DownArrow)
+            {
+                pos++;
+            }
+             else if (key.Key == ConsoleKey.Escape)
+            {
+                return -1;
+            }
+
+            Console.SetCursorPosition(0, pos);
+            Console.WriteLine("=>");
+            key = Console.ReadKey();
+        }
+
+        return pos;
+
+
+    }
+}
